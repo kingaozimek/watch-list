0d04f71e53d3281eac05c79f4af409d5e6ca85b4
Kinga Ozimek <kingaozime@gmail.com>
2022-02-17 14:52:13 +0100

    new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_120748) do
+ActiveRecord::Schema.define(version: 2022_02_17_134914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2022_02_17_120748) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "list_id"
+    t.integer "movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
