b0dd85ed881296021f0a101b8f3adbf7bf6c909b
Kinga Ozimek <kingaozime@gmail.com>
2022-02-17 15:27:42 +0100

    rails

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,16 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+{
+  "application.js": "/packs/js/application-96789fa154ddd17c2884.js",
+  "application.js.map": "/packs/js/application-96789fa154ddd17c2884.js.map",
+  "entrypoints": {
+    "application": {
+      "js": [
+        "/packs/js/application-96789fa154ddd17c2884.js"
+      ],
+      "js.map": [
+        "/packs/js/application-96789fa154ddd17c2884.js.map"
+      ]
+    }
+  }
+}
